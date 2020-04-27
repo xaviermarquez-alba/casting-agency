21fd1220fc06a551f40461bfa183e763de41ecba
Xavier <xaviermarquez.alba@gmail.com>
2020-04-26 23:48:07 -0300

    clean files

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    # db_drop_and_create_all()
+    #db_drop_and_create_all()
 
 '''
     drops the database tables and starts fresh
